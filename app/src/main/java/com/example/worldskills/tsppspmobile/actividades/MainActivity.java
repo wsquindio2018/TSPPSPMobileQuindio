2e53e589e7e492dffbc2231afb668fe262cc36e6
WORLDSKILLS <juandiego1918@gmail.com>
2018-09-01 09:25:02 -0500

    Juan

--- app/src/main/java/com/example/worldskills/tsppspmobile/actividades/MainActivity.java
+++ app/src/main/java/com/example/worldskills/tsppspmobile/actividades/MainActivity.java
@@ -1,15 +1,99 @@
 package com.example.worldskills.tsppspmobile.actividades;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.worldskills.tsppspmobile.R;
+import com.example.worldskills.tsppspmobile.adapter.AdapterProyect;
+import com.example.worldskills.tsppspmobile.entidades.ProjectPrincipal;
+import com.example.worldskills.tsppspmobile.entidades.ProjectVo;
+import com.example.worldskills.tsppspmobile.utilidades.Conexion;
+import com.example.worldskills.tsppspmobile.utilidades.Utilidades;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Conexion conn;
+    SQLiteDatabase bd;
+    EditText nombre;
+    RecyclerView recyclerView;
+    ProjectVo miProjectVo;
+    ArrayList<ProjectVo> list;
+    int id=0;
+    String campoNombre;
+    AdapterProyect adapterProyect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Se instancia la clase Conexion para trabajar con la base de datos
+        conn=new Conexion(getApplicationContext(),"TSP",null,1);
+
+        //Se instancia el campo de texto para capturar el valor ingresado
+        nombre=findViewById(R.id.campoNombre);
+        recyclerView=findViewById(R.id.recycler);
+        list=new ArrayList<>();
+        consulta();
+    }
+
+    private void consulta() {
+        list.clear();
+        bd=conn.getReadableDatabase();
+
+        Cursor cursor=bd.rawQuery("SELECT * FROM "+Utilidades.TABLA_PROJECT,null);
+
+        while (cursor.moveToNext()){
+            miProjectVo=new ProjectVo();
+            miProjectVo.setNombre_projecto(cursor.getString(1));
+            miProjectVo.setId(cursor.getInt(0));
+            id=cursor.getInt(0)+1;
+            list.add(miProjectVo);
+        }
+
+        adapterProyect=new AdapterProyect(list);
+
+        recyclerView.setAdapter(adapterProyect);
+
+        adapterProyect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProjectPrincipal.id=list.get(recyclerView.getChildAdapterPosition(v)).getId();
+                Intent miIntent=new Intent(MainActivity.this,MenuOpciones.class);
+                startActivity(miIntent);
+            }
+        });
+
+    }
+
+    public void onClick(View view) {
+        campoNombre=nombre.getText().toString();
+        bd=conn.getWritableDatabase();
+
+        ContentValues values=new ContentValues();
+        values.put(Utilidades.CAMPO_ID,id);
+        values.put(Utilidades.CAMPO_NOMBRE_PROJECT,campoNombre);
+
+        if (campoNombre.equals("")){
+            Toast.makeText(getApplicationContext(),"Debe llenar todos los campos",Toast.LENGTH_SHORT).show();
+        }else {
+            bd.insert(Utilidades.TABLA_PROJECT,Utilidades.CAMPO_ID,values);
+            limpiar();
+        }
+
+        consulta();
+    }
+
+    private void limpiar() {
+        nombre.setText(null);
     }
 }
--- app/src/main/java/com/example/worldskills/tsppspmobile/adapter/AdapterProyect.java
+++ app/src/main/java/com/example/worldskills/tsppspmobile/adapter/AdapterProyect.java
@@ -1,28 +1,62 @@
 package com.example.worldskills.tsppspmobile.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class AdapterProyect extends RecyclerView.Adapter<AdapterProyect.ProjectoHolderView> {
+import com.example.worldskills.tsppspmobile.R;
+import com.example.worldskills.tsppspmobile.entidades.ProjectVo;
+
+import java.util.ArrayList;
+
+public class AdapterProyect extends RecyclerView.Adapter<AdapterProyect.ProjectoHolderView> implements View.OnClickListener {
+
+    ArrayList<ProjectVo> lista;
+    View.OnClickListener listener;
+
+    //contructor que recible el array list de tipo vo
+    public AdapterProyect(ArrayList<ProjectVo> lista) {
+        this.lista = lista;
+    }
+
     @Override
     public ProjectoHolderView onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.modelo, null);
+        view.setOnClickListener(this);
+        return new ProjectoHolderView(view);
     }
 
     @Override
     public void onBindViewHolder(ProjectoHolderView holder, int position) {
-
+        holder.nombre.setText(lista.get(position).getNombre_projecto());
+        holder.id.setText(""+lista.get(position).getId());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lista.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (listener != null) {
+            listener.onClick(v);
+        }
     }
 
     public class ProjectoHolderView extends RecyclerView.ViewHolder {
+        TextView id, nombre;
+
         public ProjectoHolderView(View itemView) {
             super(itemView);
+            id = itemView.findViewById(R.id.idModelo);
+            nombre = itemView.findViewById(R.id.nombreModelo);
         }
     }
 }
